b1fa4f35f12dbb8a83af3179d0622c5045fd7a68
chandralekha <chandralekha@wiinnova.com>
2017-10-06 17:08:35 +0530

    b2

---
+++ test new/view2ViewController.m
@@ -0,0 +1,38 @@
+//
+//  view2ViewController.m
+//  test new
+//
+//  Created by Wiinnova Mac User1 on 06/10/2017.
+//  Copyright © 2017 wiinnova. All rights reserved.
+//
+
+#import "view2ViewController.h"
+
+@interface view2ViewController ()
+
+@end
+
+@implementation view2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"sdfsd");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
