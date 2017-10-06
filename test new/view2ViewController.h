3fc8b7522a6e09a05b00094fb844e249004d2899
chandralekha <chandralekha@wiinnova.com>
2017-10-06 17:07:10 +0530

    master3

---
+++ test new/view2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  view2ViewController.h
+//  test new
+//
+//  Created by Wiinnova Mac User1 on 06/10/2017.
+//  Copyright © 2017 wiinnova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface view2ViewController : UIViewController
+
+@end
