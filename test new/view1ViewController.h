639a5f6dce1965a98a868e66c0731f998a585d1d
chandralekha <chandralekha@wiinnova.com>
2017-10-06 17:02:14 +0530

    master1

---
+++ test new/view1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  view1ViewController.h
+//  test new
+//
+//  Created by Wiinnova Mac User1 on 06/10/2017.
+//  Copyright © 2017 wiinnova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface view1ViewController : UIViewController
+
+@end
---
+++ test new/view1ViewController.m
@@ -0,0 +1,37 @@
+//
+//  view1ViewController.m
+//  test new
+//
+//  Created by Wiinnova Mac User1 on 06/10/2017.
+//  Copyright © 2017 wiinnova. All rights reserved.
+//
+
+#import "view1ViewController.h"
+
+@interface view1ViewController ()
+
+@end
+
+@implementation view1ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
